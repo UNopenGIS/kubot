1a262b2a2bba8fb7b658dd3510c3307801e572de
Hidenori FUJIMURA <hfu@users.noreply.github.com>
2024-05-05 08:58:58 +0900

    update

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Discord bot to report the status of kubo
 
 # How to use
-1. Set your `BOT_TOKEN`, `BOT_NAME` and `CHANNEL_ID` on `.env`.
+1. Set your `BOT_TOKEN`, and `CHANNEL_ID` on `.env`.
 2. `make bot`
 
 # Required ruby libraries
--- bot.rb
+++ bot.rb
@@ -1,20 +1,21 @@
 require 'dotenv/load'
 require 'discordrb'
 
-bot_token = ENV['BOT_TOKEN']
-bot_name = ENV['BOT_NAME']
-channel_id = ENV['CHANNEL_ID']
+$bot_token = ENV['BOT_TOKEN']
+$bot_name = `ipfs id | jq .ID | ruby -e 'puts gets.strip[-5..-2]'`.strip
+$channel_id = ENV['CHANNEL_ID']
 
-bot = Discordrb::Bot.new(token: bot_token)
+bot = Discordrb::Bot.new(token: $bot_token)
 
 bot.ready {
-  bot.send_message(channel_id, "#{bot_name} is up!")
+  bot.send_message($channel_id, "#{$bot_name} is up!")
 }
 
 bot.message {|e|
   unless e.author.bot_account?
     if e.content.include?(bot.profile.mention)
-      m = `ipfs stats bw`.to_s.sub("Bandwidth\n", "").gsub("\n", " ").chomp
+      m = "#{$bot_name}: "
+      m += `ipfs stats bw`.to_s.sub("Bandwidth\n", "").gsub("\n", " ").chomp
       e.respond(m)
     end
   end
