6e9b8f9205c246467ab3ae95682e7cbada321287
Hidenori <hfu@users.noreply.github.com>
2024-05-06 07:46:24 +0900

    update

--- bot.rb
+++ bot.rb
@@ -2,7 +2,8 @@ require 'dotenv/load'
 require 'discordrb'
 
 $bot_token = ENV['BOT_TOKEN']
-$bot_name = `ipfs id | jq .ID | ruby -e 'puts gets.strip[-5..-2]'`.strip
+$bot_name = `ipfs id | jq .ID | ruby -e 'puts gets.strip[-5..-2]'`.strip + 
+  "/#{`hostname -s`.strip}"
 $channel_id = ENV['CHANNEL_ID']
 
 bot = Discordrb::Bot.new(token: $bot_token)
@@ -14,7 +15,7 @@ bot.ready {
 bot.message {|e|
   unless e.author.bot_account?
     if e.content.include?(bot.profile.mention)
-      m = "#{$bot_name}/#{`hostname -s`}: "
+      m = "#{$bot_name}: "
       m += `ipfs stats bw`.to_s.sub("Bandwidth\n", "").gsub("\n", " ").chomp
       e.respond(m)
     end
